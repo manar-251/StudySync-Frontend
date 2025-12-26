cd5332f8f4221dc9441390601293cc006e761b6d
Manar's <manarmohd15@gmail.com>
2025-12-27 01:11:52 +0300

    creat empty form

--- src/components/Wellness/Wellness.jsx
+++ src/components/Wellness/Wellness.jsx
@@ -2,5 +2,13 @@ import { useEffect, useState } from 'react';
 import * as wellnessLogService from '../../services/wellnessLogService';
 import styles from './Wellness.module.css';
 
+const emptyForm = {
+  date: '',
+  waterGlasses: 0,
+  exerciseMinutes: 0,
+  sleepHours: 0,
+};
+
+
 
 export default Wellness;
