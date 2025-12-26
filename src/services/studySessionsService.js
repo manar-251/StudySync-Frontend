67fbf25135cb91531c77fc47040a6cabe74383ae
Manar's <manarmohd15@gmail.com>
2025-12-27 00:22:08 +0300

    creat session

--- src/services/studySessionsService.js
+++ src/services/studySessionsService.js
@@ -1,5 +1,12 @@
 import { sendRequest } from './sendRequest';
-
 const index = () => sendRequest('/studySessions');
+
+
+const create = (sessionFormData) =>
+  sendRequest('/studySessions', {
+    method: 'POST',
+    body: sessionFormData,
+  });
+
 
 export { index, create, remove };
