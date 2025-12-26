b2d41020c95337a30f18c09caef358e3902556db
Manar's <manarmohd15@gmail.com>
2025-12-26 18:38:14 +0300

    editing app.jsx, removing hoots things

--- src/App.jsx
+++ src/App.jsx
@@ -5,70 +5,49 @@ import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
 import SignupForm from './components/SignupForm/SignupForm';
 import SigninForm from './components/SigninForm/SigninForm';
-import HootList from './components/HootList/HootList';
-import HootDetails from './components/HootDetails/HootDetails';
-import HootForm from './components/HootForm/HootForm';
-import CommentForm from './components/CommentForm/CommentForm';
+import Tasks from './components/Tasks/Tasks';
 import * as authService from '../src/services/authService';
-import * as hootService from './services/hootService';
+
 
 const App = () => {
   const [user, setUser] = useState(authService.getUser());
-  const [hoots, setHoots] = useState([]);
 
-  const navigate = useNavigate();
 
   const handleSignout = () => {
     authService.signout();
     setUser(null);
   };
 
-  const handleAddHoot = async (hootFormData) => {
-    const newHoot = await hootService.create(hootFormData);
-    setHoots([...hoots, newHoot]);
-    navigate('/hoots');
-  };
 
-  const handleDeleteHoot = async (hootId) => {
-    const deletedHoot = await hootService.remove(hootId);
-    setHoots(hoots.filter((hoot) => hoot._id !== deletedHoot._id));
-    navigate('/hoots');
-  };
-
-  const handleUpdateHoot = async (hootId, hootFormData) => {
-    const updatedHoot = await hootService.update(hootId, hootFormData);
-    setHoots(hoots.map((hoot) => (hootId === hoot._id ? updatedHoot : hoot)));
-    navigate(`/hoots/${hootId}`);
-  };
-
-  useEffect(() => {
-    const getHoots = async () => {
-      const hootData = await hootService.index();
-      setHoots(hootData);
-    };
-    if (user) {
-      getHoots();
-    };
-  }, [user]);
 
   return (
     <>
       <NavBar user={user} handleSignout={handleSignout} />
       <Routes>
-        { user ? (
-          <>
-          <Route path="/" element={<Dashboard user={user} />} />
-          <Route path="/hoots" element={<HootList hoots={hoots} />} />
-          <Route path="/hoots/:hootId" element={<HootDetails user={user} handleDeleteHoot={handleDeleteHoot} />} />
-          <Route path="/hoots/new" element={<HootForm handleAddHoot={handleAddHoot} />} />
-          <Route path="/hoots/:hootId/edit" element={<HootForm handleUpdateHoot={handleUpdateHoot} />} />
-          <Route path="/hoots/:hootId/comments/:commentId/edit" element={<CommentForm />} />
-          </>
-        ) : (
-          <Route path="/" element={<Landing />} />
-        )}
-        <Route path='/signup' element={<SignupForm setUser={setUser} />} />
-        <Route path='/signin' element={<SigninForm setUser={setUser} />} />
+        <Route path="/" element={user ? <Navigate to="/dashboard" replace /> : <Landing />} />
+        <Route path="/signup" element={<SignupForm setUser={setUser} />} />
+        <Route path="/signin" element={<SigninForm setUser={setUser} />} />
+        
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute user={user}>
+              <Dashboard user={user} />
+            </ProtectedRoute>
+          }
+        />
+
+       
+        
+          <Route
+          path="/profile"
+          element={
+            <ProtectedRoute user={user}>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+
       </Routes>
     </>
   );
