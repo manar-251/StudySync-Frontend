ede8b3870c55ed62b6faff2533ca3e79da49558b
Manar's <manarmohd15@gmail.com>
2025-12-27 01:11:12 +0300

    import things

---
+++ src/components/Wellness/Wellness.jsx
@@ -0,0 +1,6 @@
+import { useEffect, useState } from 'react';
+import * as wellnessLogService from '../../services/wellnessLogService';
+import styles from './Wellness.module.css';
+
+
+export default Wellness;
