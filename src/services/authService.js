f499c25faf9502407d6f47136190e2ae3c087ef8
Manar's <manarmohd15@gmail.com>
2025-12-26 18:51:40 +0300

    Auth, sending req

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,19 +1,15 @@
 const signup = async (formData) => {
-  try {
-    const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/register`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(formData),
-    });
-    const json = await res.json();
-    if (json.err) {
-      throw new Error(json.err);
-    }
-    return json;
-  } catch (err) {
-    console.log(err);
-    throw err;
-  };
+  const res = await sendRequest('/auth/sign-up', {
+    method: 'POST',
+    body: formData,
+  });
+
+  if (res?.token) {
+    setToken(res.token);
+    return getUser();
+  }
+
+  return null;
 };
 
 const signin = async (user) => {
